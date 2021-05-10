23a794f1e1b1c416365d404d7270b25ff6704b9f
si1kn <si1kn521@gmail.com>
2021-05-10 12:34:46 +1000

    Setup Basic Plugin

---
+++ src/main/java/io/github/si1kn/bpp/BPPlugin.java
@@ -0,0 +1,15 @@
+package io.github.si1kn.bpp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BPPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
